5270f8e27aa73a3c88d6e2331d1802513dbaa322
Aniket Pradhan <aniketpradhan814@gmail.com>
2022-05-05 10:30:25 +0530

    remove no-cors mode

--- src/services/index.js
+++ src/services/index.js
@@ -2,8 +2,8 @@ const API_URL = `https://api.domainsdb.info/v1/domains/search?domain=facebook`;
 
 export async function getDomains() {
   try {
-    const response = await fetch(`${API_URL}`, {mode:'no-cors'});
-    const data = await response.json();
+    const response = await fetch(`${API_URL}`);
+    const data = await response.text();
     console.log("data", data)
     return data;
   } catch (error) {
